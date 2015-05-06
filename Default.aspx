264c5df5e6d12ada7a6015325a0de83109b9bffb
Ali Gamal <ali.gamal1987@gmail.com>
2015-05-06 16:28:53 +0300

    RTL - Bootstrap
    RTL - Bootstrap

---
+++ Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainPlaceHolder" Runat="Server">
+     <section class="bs-docs-masthead" id="content" role="main" tabindex="-1">
+            <div class="container">
+                <span class="bs-docs-booticon bs-docs-booticon-lg bs-docs-booticon-outline">B</span>
+                <p class="lead">Bootstrap Documentaion</p>
+                <p class="version">
+                    Guid for developers showing how to use bootstrap v3.3.4
+                <br />
+                    By Naseej UI E-Solution Team
+                </p>
+            </div>
+        </section>
+</asp:Content>
+
---
+++ js/utilities.js
@@ -0,0 +1,42 @@
+﻿
+$(function () {
+    var mainPlaceHolder = $("#mainPlaceHolder");
+    mainPlaceHolder.load("main.html");
+    /* single page application using jQuery & Ajax */
+
+    $("body").on('click', '*[data-url]', function (e) {
+        e.preventDefault();
+        var siteUrl = $(this).attr("data-url"); // get url
+
+        $.ajax({
+            type: "GET",
+            url: siteUrl,
+            dataType: "html",
+            beforeSend: function () {
+                mainPlaceHolder.html("<div class='preloader'><img src='images/prealoader.gif' /></div>");
+
+            },
+        })
+        .done(function (html) {
+            mainPlaceHolder.html(html);
+        })
+        .fail(function () {
+            //alert( "error" );
+        })
+        .success(function () {
+            //alert("success");
+        })
+    });
+
+
+    $(window).scroll(function () {
+        /* scroll top arrow */
+        if ($(this).scrollTop() > 500) {
+            $('.bs-docs-sidebar').addClass("affix");
+        } else {
+            $('.bs-docs-sidebar').removeClass("affix");
+        }
+    });
+
+
+})
